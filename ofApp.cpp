7e5b89745d5c3badf0bc22e8cddf84c4d325faa3
NAKAUCHI Kiyoshi <junkiyoshi@gmail.com>
2020-01-11 14:07:13 +0900

    Add files via upload

--- ofApp.cpp
+++ ofApp.cpp
@@ -3,17 +3,18 @@
 //--------------------------------------------------------------
 void ofApp::setup() {
 
-	ofSetFrameRate(60);
+	ofSetFrameRate(30);
 	ofSetWindowTitle("openframeworks");
 
 	ofBackground(239);
+	ofEnableDepthTest();
 
-	this->font.loadFont("fonts/Kazesawa-Bold.ttf", 28);
+	this->font.loadFont("fonts/Kazesawa-Bold.ttf", 30, true, true, true);
 
-	for (int i = 0; i < 750; i++) {
+	for (int i = 0; i < 300; i++) {
 
-		auto location = glm::vec3(ofRandom(-300, 300), ofRandom(-300, 300), ofRandom(-300, 300));
-		location = glm::normalize(location) * ofRandom(200, 600);
+		auto location = glm::vec3(ofRandom(-1, 1), ofRandom(-1, 1), ofRandom(-1, 1));
+		location = glm::normalize(location) * ofRandom(150, 200);
 		this->location_list.push_back(location);
 	}
 
@@ -54,17 +55,48 @@ void ofApp::draw() {
 
 	for (int i = 0; i < this->location_list.size(); i++) {
 
-		i == this->target_index ? ofSetColor(239, 0, 0) : ofSetColor(39);
-
 		ofPushMatrix();
 		ofTranslate(this->location_list[i]);
 		ofRotateX(ofMap(ofNoise(ofRandom(10000), ofGetFrameNum() * 0.005), 0, 1, -180, 180));
 		ofRotateY(ofMap(ofNoise(ofRandom(10000), ofGetFrameNum() * 0.005), 0, 1, -180, 180));
 		ofRotateZ(ofMap(ofNoise(ofRandom(10000), ofGetFrameNum() * 0.005), 0, 1, -180, 180));
 
+		
+		char noise_value = ofMap(ofNoise(this->location_list[i].x * 0.008, this->location_list[i].y * 0.008, this->location_list[i].z * 0.008, ofGetFrameNum() * 0.01), 0, 1, '0', 'Z');
+		auto path = font.getCharacterAsPoints(noise_value, true, false);
+		auto outline = path.getOutline();
 
-		char noise_value = ofMap(ofNoise(this->location_list[i].x * 0.008, this->location_list[i].y * 0.008, this->location_list[i].z * 0.008, ofGetFrameNum() * 0.005), 0, 1, 'A', 'Z');
-		this->font.drawString({ noise_value }, -14, -14);
+		ofFill();
+		i == this->target_index ? ofSetColor(239, 0, 0) : ofSetColor(239);
+		ofBeginShape();
+		for (int outline_index = 0; outline_index < outline.size(); outline_index++) {
+
+			if (outline_index != 0) { ofNextContour(true); }
+
+			auto vertices = outline[outline_index].getVertices();
+			for (int vertices_index = 0; vertices_index < vertices.size(); vertices_index++) {
+
+				auto location = glm::vec2(vertices[vertices_index].x - this->font.stringWidth(string{ noise_value }) * 0.5, vertices[vertices_index].y + this->font.stringHeight(string{ noise_value }) * 0.5);
+				ofVertex(location);
+			}
+		}
+		ofEndShape(true);
+
+		ofNoFill();
+		ofSetColor(39);
+		ofBeginShape();
+		for (int outline_index = 0; outline_index < outline.size(); outline_index++) {
+
+			if (outline_index != 0) { ofNextContour(true); }
+
+			auto vertices = outline[outline_index].getVertices();
+			for (int vertices_index = 0; vertices_index < vertices.size(); vertices_index++) {
+
+				auto location = glm::vec2(vertices[vertices_index].x - this->font.stringWidth(string{ noise_value }) * 0.5, vertices[vertices_index].y + this->font.stringHeight(string{ noise_value }) * 0.5);
+				ofVertex(location);
+			}
+		}
+		ofEndShape(true);
 
 		ofPopMatrix();
 	}
